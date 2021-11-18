4419046c2512dea8f45a14b99acae421a5c16e8d
AdDy Dzankic <adidzankic@gmail.com>
2021-11-18 15:09:24 +0100

    React Project

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from "./containers";
+import { Cta, Brand, NavBar } from "./components"; 
+
+import "./App.css";
+
+const App = () => {
+    return (
+        <div className="App">
+            <div className="gradient__bg">
+            <NavBar />
+            <Header />
+            </div>
+         <Brand />
+         <WhatGPT3 />
+         <Features />
+         <Possibility />
+         <Cta />
+         <Blog />
+         <Footer />
+        </div>
+    )
+}
+
+export default App;
